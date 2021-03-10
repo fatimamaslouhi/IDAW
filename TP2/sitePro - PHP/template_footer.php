bd9c862ca0641d6da2968ff9bf758c6453d57fb1
Fatima Ez-zahrae MASLOUHI <fatimaezzahraemaslouhi@gmail.com>
2021-03-10 11:48:46 +0100

    header+footer

---
+++ TP2/sitePro - PHP/template_footer.php
@@ -0,0 +1,8 @@
+<DOCTYPE html>
+    <html>
+        <body>
+        <footer>
+                    <p>Mise en page © Copyright ^^^^, by Fatima MASLOUHI</p>
+        </footer>
+    </body>
+    </html>
---
+++ TP2/sitePro - PHP/template_header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+    <head>
+    <title>Fatima MASLOUHI</title>
+    <link rel="stylesheet" href="habillage.css" type="text/css" media="screen" title="default" charset="utf-8">
+    </head>
+    <body>
