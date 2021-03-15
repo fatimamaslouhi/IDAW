1bfdf60b37d593bd6ff9d373df6a93cbf55bfc50
Fatima Ez-zahrae MASLOUHI <fatimaezzahraemaslouhi@gmail.com>
2021-03-15 17:58:04 +0100

    TP 3

---
+++ TP3/partie2 BDD/testConnexion.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test Connexion BDD</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            $bd='webdev';
+            
+            //On établit la connexion
+            $mysqli = new mysqli($servername, $username, $password,$bd);
+            
+            //On vérifie la connexion
+            if($mysqli->connect_error){
+                die('Erreur : ' .$mysqli->connect_error);
+            }
+            echo 'Connexion réussie';
+
+            $result = $mysqli->query('SELECT id,pseudo FROM connexion;') ;
+
+            if (!$result) {
+              die('<p>ERREUR Requête invalide : '.$mysqli->error.'</p>');
+            }
+            echo '<br><br><table border=3>
+                    <tr><th>Identifiant</th><th>Nom & pr&eacutenom</th></tr>';
+            for ($i=0 ; $i < $result->num_rows ; $i++) {
+              $row = $result->fetch_assoc() ;
+              $id = $row['id'] ;
+              $pseudo = $row['pseudo'] ;
+              echo '<tr><td>'.$id.'</td><td> '.$pseudo.'</td>'."\r\n" ;
+            }
+            echo '</table>';
+            $result->free() ;
+            
+            $result = $mysqli->query('INSERT INTO connexion(`id`, `login`, `password`, `pseudo`) 
+            VALUES (5,\'jacques.roux\',\'pswd5\',\'Jacques Roux\');');
+            $result->free() ;
+            $mysqli->close() ;
+        ?>
+    </body>
+</html>
